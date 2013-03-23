78d5425b2a239c9546690917bfda4788bbac6c8e
Desmond Kirrane <desmond.kirrane@gmail.com>
2013-03-23 19:06:06 +0000

    Initial Project commit

---
+++ common-java-pom/mavenproject1/src/main/java/com/mycompany/app/mavenproject1/App.java
@@ -0,0 +1,12 @@
+package com.mycompany.app.mavenproject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
